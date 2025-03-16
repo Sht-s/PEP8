b030a1477f76ec3bc61fa40d4e4235e94b1e6604
Edgar R <63327227+Sht-s@users.noreply.github.com>
2025-03-15 19:00:11 -0600

    Archivos agregados directamente en github.com

---
+++ PostPEP8.py
@@ -0,0 +1,21 @@
+def shellsort(input_list):
+    gap = len(input_list)//2
+    while gap > 0:
+        for i in range(gap, len(input_list)):
+            temp = input_list[i]
+            j = i
+            # ordena la sublista para este gap
+            while j >= gap and input_list[j - gap] > temp:
+                input_list[j] = input_list[j - gap]
+                j = j-gap
+            input_list[j] = temp
+        # reduce el gap para el elemento que sigue
+        gap = gap//2
+
+list = []
+n = int(input("¿Cuántos valores desea agregar a la lista?: "))
+for i in range(0, n):
+    list.append(int(input("Teclee el siguiente valor a ingresar: ")))
+print("Lista desordenada: ", list)
+shellsort(list)
+print("\nLista ordenada: ", list)
